eb2651b501324d3d920d8edca9cd8f7f874ded8a
hc0re <hc0re@tuta.io>
2022-07-06 15:07:31 +0200

    fixes in fish script

--- provisioning-solus.fish
+++ provisioning-solus.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/fish
 
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
 omf install dangerous
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
@@ -11,3 +13,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fish_add_path ~/.cargo/bin/
 
 curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+
+set -ga fish_user_paths ~/.nimble/bin
--- provisioning-solus.sh
+++ provisioning-solus.sh
@@ -19,21 +19,21 @@ if [[ ! -d ~/.local/bin ]]; then
     mkdir -p ~/.local/bin
 fi
 
-#URL=$(http --headers https://github.com/rust-lang/rust-analyzer/releases/latest | grep Location | awk -F ' ' '{print $2}')
-#FIXED_URL=$(echo $URL | sed 's/\r//' | sed 's/tag/download/g')
-#curl -L -o rust-analyzer-x86_64-unknown-linux-gnu.gz "${FIXED_URL}/rust-analyzer-x86_64-unknown-linux-gnu.gz"
-#gzip -d rust-analyzer-x86_64-unknown-linux-gnu.gz
-#mv -f rust-analyzer-x86_64-unknown-linux-gnu ~/.local/bin/rust-analyzer
-#chmod +x ~/.local/bin/rust-analyzer
+URL=$(http --headers https://github.com/rust-lang/rust-analyzer/releases/latest | grep Location | awk -F ' ' '{print $2}')
+FIXED_URL=$(echo $URL | sed 's/\r//' | sed 's/tag/download/g')
+curl -L -o rust-analyzer-x86_64-unknown-linux-gnu.gz "${FIXED_URL}/rust-analyzer-x86_64-unknown-linux-gnu.gz"
+gzip -d rust-analyzer-x86_64-unknown-linux-gnu.gz
+mv -f rust-analyzer-x86_64-unknown-linux-gnu ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
 
-#echo 'configuring neovim'
+echo 'configuring neovim'
 
-#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-#if [[ ! -d ~/.config/nvim ]]; then
-#    git clone https://github.com/xhc0re/nvim ~/.config/nvim
-#fi
+if [[ ! -d ~/.config/nvim ]]; then
+    git clone https://github.com/xhc0re/nvim ~/.config/nvim
+fi
 
 git config --global user.name "hc0re"
 git config --global user.email "hc0re@tuta.io"
